e183dc06da9d97a56f088b31b24376c830698ebf
Bombohog <70882697+Bombohog@users.noreply.github.com>
2020-09-28 11:12:20 +0200

    ClassTest exercise

--- src/Exercises_28_09_2020/ClassTest.java
+++ src/Exercises_28_09_2020/ClassTest.java
@@ -17,26 +17,35 @@ public class ClassTest {
 
         Student[] students = StudentCreation();
 
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the name, middlename(if any) or a lastname of a person in your student group: ");
+        while (true) {
+            Scanner input = new Scanner(System.in);
+            System.out.print("Enter the name, middlename(if any) or a lastname of a person in your student group: ");
 
-        String enteredInput = input.nextLine();
-        
-        switch (enteredInput.toLowerCase()) {
-            case "lasse" -> System.out.println("Name: " + students[0].name + " " + students[0].middleName + " " + students[0].lastName +
-                    "\nAge: " + students[0].age + "\nLives in: " + students[0].city + "\nAcademics: " + students[0].academics);
-            case "fredrik" -> System.out.println("Name: " + students[1].name + " " + students[1].middleName + " " + students[1].lastName +
-                    "\nAge: " + students[1].age + "\nLives in: " + students[1].city + "\nAcademics: " + students[1].academics);
-            case "deniz" -> System.out.println("Name: " + students[2].name + " " + students[2].middleName + " " + students[2].lastName +
-                    "\nAge: " + students[2].age + "\nLives in: " + students[2].city + "\nAcademics: " + students[2].academics);
-            case "simon" -> System.out.println("Name: " + students[3].name + " " + students[3].middleName + " " + students[3].lastName +
-                    "\nAge: " + students[3].age + "\nLives in: " + students[3].city + "\nAcademics: " + students[3].academics);
-            default -> {
-                for (Student i : students) {
-                    if (enteredInput.equals(i.middleName) || enteredInput.equals(i.lastName)) {
-                        System.out.println("Name: " + i.name + " " + i.middleName + " " + i.lastName +
-                                "\nAge: " + i.age + "\nLives in: " + i.city + "\nAcademics: " + i.academics);
-                    } else {
+            String enteredInput = input.nextLine();
+
+            switch (enteredInput.toLowerCase()) {
+                case "lasse" -> System.out.println("Name: " + students[0].name + " " + students[0].middleName + " " + students[0].lastName +
+                        "\nAge: " + students[0].age + "\nLives in: " + students[0].city + "\nAcademics: " + students[0].academics);
+                case "fredrik" -> System.out.println("Name: " + students[1].name + " " + students[1].middleName + " " + students[1].lastName +
+                        "\nAge: " + students[1].age + "\nLives in: " + students[1].city + "\nAcademics: " + students[1].academics);
+                case "deniz" -> System.out.println("Name: " + students[2].name + " " + students[2].middleName + " " + students[2].lastName +
+                        "\nAge: " + students[2].age + "\nLives in: " + students[2].city + "\nAcademics: " + students[2].academics);
+                case "simon" -> System.out.println("Name: " + students[3].name + " " + students[3].middleName + " " + students[3].lastName +
+                        "\nAge: " + students[3].age + "\nLives in: " + students[3].city + "\nAcademics: " + students[3].academics);
+                default -> {
+                    int invalidOrNot = students.length;
+
+                    for (Student i : students) {
+                        if (enteredInput.toLowerCase().equals(i.middleName.toLowerCase()) ||
+                                enteredInput.toLowerCase().equals(i.lastName.toLowerCase())) {
+                            System.out.println("Name: " + i.name + " " + i.middleName + " " + i.lastName +
+                                    "\nAge: " + i.age + "\nLives in: " + i.city + "\nAcademics: " + i.academics);
+                            break;
+                        } else {
+                            invalidOrNot -= 1;
+                        }
+                    }
+                    if (invalidOrNot == 0) {
                         System.out.println("Invalid input!");
                     }
                 }
@@ -47,37 +56,33 @@ public class ClassTest {
 
     public static Student[] StudentCreation() {
         
-        Student lasse = new Student();
-        lasse.name = "Lasse";
+        Student lasse = new Student("Lasse", "Jørgensen", "Kongsdal", 22, "Skuderløse");
+        /*lasse.name = "Lasse";
         lasse.middleName = "Jørgensen";
         lasse.lastName = "Kongsdal";
         lasse.age = 22;
-        lasse.city = "Skuderløse";
-        lasse.academics = "AP Graduate";
+        lasse.city = "Skuderløse";*/
 
-        Student fredrik = new Student();
-        fredrik.name = "Fredrik";
+        Student fredrik = new Student("Fredrik", "", "Bille", 24, "Nykøbing Falster");
+        /*fredrik.name = "Fredrik";
         fredrik.middleName = "";
         fredrik.lastName = "Bille";
         fredrik.age = 24;
-        fredrik.city = "Nykøbing Falster";
-        fredrik.academics = "AP Graduate";
+        fredrik.city = "Nykøbing Falster";*/
 
-        Student deniz = new Student();
-        deniz.name = "Deniz";
+        Student deniz = new Student("Deniz", "Yahya", "Yandim", 20, "Vordingborg");
+        /*deniz.name = "Deniz";
         deniz.middleName = "Yahya";
         deniz.lastName = "Yandim";
         deniz.age = 20;
-        deniz.city = "Vordingborg";
-        deniz.academics = "AP Graduate";
+        deniz.city = "Vordingborg";*/
 
-        Student simon = new Student();
-        simon.name = "Simon";
+        Student simon = new Student("Simon", "Stentoft", "Heegaard", 24, "Faxe");
+        /*simon.name = "Simon";
         simon.middleName = "Stentoft";
         simon.lastName = "Heegaard";
         simon.age = 24;
-        simon.city = "Faxe";
-        simon.academics = "AP Graduate";
+        simon.city = "Faxe";*/
 
         return new Student[]{lasse, fredrik, deniz, simon};
     }
@@ -93,4 +98,22 @@ class Student {
     public String city;
     public String academics;
 
+    Student() {
+        academics = "AP Graduate";
+    }
+
+    Student(String name, String middleName, String lastName, int age, String city) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+        this.age = age;
+        this.city = city;
+        this.academics = "AP Graduate";
+    }
+
+    Student(String name, String middleName, String lastName) {
+        this.name = name;
+        this.middleName = middleName;
+        this.lastName = lastName;
+    }
 }
